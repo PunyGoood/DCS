37772d7dc02114a2f3c5796e05724db78ac2afb3
MaisJet <2283641711@qq.com>
2025-02-27 11:25:39 +0800

    some new

--- system/grpc/client/client.go
+++ system/grpc/client/client.go
@@ -4,7 +4,11 @@ import (
 
 	"sync"
 	"time"
-
+    "context"
+    "crypto/tls"
+    "google.golang.org/grpc"
+    "google.golang.org/grpc/credentials"
+    "github.com/cenkalti/backoff"
 	grpc2 "github.com/PunyGoood/DCS/grpc/proto"
 
 
@@ -12,16 +16,17 @@ import (
 
 type GrpcClient struct {
 
-	nodecfg *NodeConfig
+	nodeCfg *NodeConfig
 	conn *grpc.ClientConn
 	once sync.Once
 
 	address string
-
+ 
 	timeout time.Duration
 	err error
 
 	substream grpc2.NodeService_SubscribeClient
+	msgCh  chan *grpc2.StreamMessage
 
 	NodeClient               grpc2.NodeServiceClient
 	TaskClient               grpc2.TaskServiceClient
@@ -30,13 +35,13 @@ type GrpcClient struct {
 
 }
 
-func (cc *GrpcClientV2) Context() (ctx context.Context, cancel context.CancelFunc) {
+func (cc *GrpcClient) Context() (ctx context.Context, cancel context.CancelFunc) {
 	return context.WithTimeout(context.Background(), cc.timeout)
 }
 
 func (cc *GrpcClientV2) Connect() (err error) {
 	op := func() error {
-		address := cC.address.String()
+		address := cc.address.String()
 
 		var opts []grpc.DialOption
 		
@@ -47,11 +52,9 @@ func (cc *GrpcClientV2) Connect() (err error) {
 		opts = append(opts, grpc.WithTransportCredentials(creds))
 		opts = append(opts, grpc.WithBlock())
 		opts = append(opts, grpc.WithTimeout(cc.timeout))
-		// 添加拦截器
 		opts = append(opts, grpc.WithChainUnaryInterceptor(auth.GetAuthTokenUnaryChainInterceptor(cc.nodeCfg)))
 		opts = append(opts, grpc.WithChainStreamInterceptor(auth.GetAuthTokenStreamChainInterceptor(cc.nodeCfg)))
 
-		// 创建gRPC连接
 		cc.conn, err = grpc.NewClient(address, opts...)
 		if err != nil {
 			_ = trace.TraceError(err)
@@ -76,7 +79,7 @@ func Start(cc *GrpcClient) {
 
 	cc.once.Do(func () {
 		err=cc.connect
-		if(err!=nil) {
+		if err != nil {
 			return
 		}
 
@@ -115,9 +118,26 @@ func (cc *GrpcClient) Stop() (err error) {
 
 func (cc *GrpcClient) NewRequest(d interface{}) (req *grpc2.Request) {
 	return &grpc2.Request{
-		NodeKey: c.nodeCfgSvc.GetNodeKey(),
-		Data:    c.getRequestData(d),
+		NodeKey: cc.nodeCfgSvc.GetNodeKey(),
+		Data:    cc.GetRequestData(d),
 	}
+}
+
+func (cc *GrpcClient) GetRequestData(d interface{}) (data []byte) {
+	if d == nil {
+		return data
+	}
+	switch d.(type) {
+	case []byte:
+		data = d.([]byte)
+	default:
+		var err error
+		data, err = json.Marshal(d)
+		if err != nil {
+			panic(err)
+		}
+	}
+	return data
 }
 
 func (cc *GrpcClient) subscribe() (err error) {
@@ -150,14 +170,66 @@ func (cc *GrpcClient) unsubscribe() (err error) {
 	return nil
 }
 
+func (cc *GrpcClient) IsClosed() (res bool) {
+	if cc.conn != nil {
+		return cc.conn.GetState() == connectivity.Shutdown
+	}
+	return false
+}
+
+func (cc *GrpcClient) manageStreamMessage() {
+	log.Infof("[GrpcClient] start managing stream message...")
+	for {
+		// resubscribe if stream is set to nil
+		if cc.stream == nil {
+			if err := backoff.RetryNotify(cc.subscribe, backoff.NewExponentialBackOff(), utils.BackoffErrorNotify("grpc client subscribe")); err != nil {
+				log.Errorf("subscribe")
+				return
+			}
+		}
+
+		//node_service内容
+		msg, err := c.stream.Recv()
+
+		// send stream message to channel
+		cc.msgCh <- msg
+
+		// receive stream message
+		log.Debugf("[GrpcClient] received message: %v", msg)
+		if err != nil {
+
+			cc.err = err
+
+			// end
+			if err == io.EOF {
+				log.Infof("[GrpcClient] received EOF signal, can not be connected")
+				return
+			}
+
+			if cc.IsClosed() {
+				return
+			}
+
+			// error
+			trace.PrintError(err)
+			c.stream = nil
+			time.Sleep(1 * time.Second)
+			continue
+		}
+
+		// reset error
+		cc.err = nil
+	}
+}
+
 func newGrpcClient() (cc *GrpcClient){
 
 	client := &GrpcClient{
 		nodecfg: nodeconfig.GetANode(),
 		timeout: 10 * time.Second,
 		address: entity.NewAddress(&entity.AddressOptions{
-			Host=cons.DefaultClientRemoteHost
-			Port=cons.DefaultClientRemotePort
+			Host : =cons.DefaultClientRemoteHost
+			Port : =cons.DefaultClientRemotePort
 		})
 
 	}
